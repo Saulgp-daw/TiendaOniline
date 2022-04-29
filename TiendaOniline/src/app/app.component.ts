9f29abdb192e4aec94d86cf0f2aa0df120b897be
saul <saulgp4a@gmail.com>
2022-04-29 23:41:40 +0100

    probando convertir json a interfaz

--- TiendaOniline/src/app/app.component.ts
+++ TiendaOniline/src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Articulo } from './model/articulo';
+//import { plainToClass } from \"class-transformer\";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    var obj: Articulo = JSON.parse("{\"id\":\"2\",\"descripcion\":\"Pack de media docena\",\"nombre\":\"Huevos\",\"precio\":\"2.40\",\"imagen\":\"https:\/\/i.imgur.com\/ty8OyAG.png\",\"categoria\":\"Alimentos\",\"estado\":\"Disponible\",\"stock\":\"40\"}");
+    console.log(obj);
+  }
   title = 'TiendaOniline';
+
+
 }
---
+++ TiendaOniline/src/app/model/articulo.ts
@@ -0,0 +1,10 @@
+export interface Articulo{
+    id: number;
+    descripcion: string;
+    nombre: string;
+    precio: number;
+    imagen: string;
+    categoria: string;
+    estado: string;
+    stock: number;
+}
