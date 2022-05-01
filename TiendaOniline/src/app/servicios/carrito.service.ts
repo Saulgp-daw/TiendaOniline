fb3f6e1baaee65e05ee6383700e4b7317d31df8b
saul <saulgp4a@gmail.com>
2022-05-02 00:01:29 +0100

    mejorando el carrito

--- TiendaOniline/src/app/servicios/carrito.service.ts
+++ TiendaOniline/src/app/servicios/carrito.service.ts
@@ -1,15 +1,44 @@
-import { Injectable, OnInit } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { Articulo } from '../model/articulo';
 
 @Injectable({
   providedIn: 'root'
 })
-export class CarritoService implements OnInit{
-  articulosCarrito: any = [];
+export class CarritoService {
+  carrito: Array<Articulo> = [];
   cantidadTotal: number = 0;
-
+  
   constructor() { }
 
-  ngOnInit(): void {
-    console.log("Prueba");
+  cargarCarrito(usuario: string = "invitado"){
+    if(this.carrito.length > 0)
+      localStorage.setItem(usuario, JSON.stringify(this.carrito));
   }
+
+  agregarACarrito(articulo: Articulo): void{
+      console.log(articulo);
+      if(this.carrito.length > 0){
+        this.carrito.map( (articuloEnCarrito: any) => {
+          if(articuloEnCarrito.id == articulo.id){
+            articuloEnCarrito.cantidad += 1;
+          }else{
+            articulo['cantidad'] = 1;
+            this.carrito.push(articulo);
+          }
+        });
+      }else{
+        articulo['cantidad'] = 1;
+        this.carrito.push(articulo);
+      }
+
+
+    this.devolverTodosArticulos();
+  }
+  
+
+  devolverTodosArticulos(){
+    console.log(this.carrito);
+  }
+
+
 }
