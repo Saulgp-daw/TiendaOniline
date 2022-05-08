bc5984bfd4ec36f2d511eb06426bb3c4bd0539ac
saul <saulgp4a@gmail.com>
2022-05-08 13:43:02 +0100

    usuario registro terminado

--- TiendaOniline/src/app/componentes/login-registro/login-registro.component.ts
+++ TiendaOniline/src/app/componentes/login-registro/login-registro.component.ts
@@ -2,13 +2,19 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder } from '@angular/forms';
 import { CrudArticulosService } from 'src/app/servicios/crud-articulos.service';
 import { firstValueFrom, lastValueFrom } from 'rxjs';
-
+/**
+ * importamos formgroup y formbuilder para recoger los valores de los inputs
+ * lastvalue from es una promesa que recogerá de la api el último los valores que nos devuelva
+ */
 
 @Component({
   selector: 'app-login-registro',
   templateUrl: './login-registro.component.html',
   styleUrls: ['./login-registro.component.css']
 })
+/**
+ * en el contructor ponemos el nombre de los campos que vayamos a recibir
+ */
 export class LoginRegistroComponent implements OnInit {
   formularioDeRegistro: FormGroup;
   public resultadoRegistro: any;
@@ -29,6 +35,10 @@ export class LoginRegistroComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  /**
+   * la variable nuevo registro se ejecutará una vez el usuario envíe sus datos a nuestra base de datos, dependiendo de la respuesta
+   * se le mostrará un mensaje con el resultado
+   */
   async nuevoRegistro(): Promise<void>{
     console.log("Prueba");
     console.log(this.formularioDeRegistro.value);
@@ -36,7 +46,7 @@ export class LoginRegistroComponent implements OnInit {
     this.resultadoRegistro = await lastValueFrom(this.crudArticuloService.agregarUsuario(this.formularioDeRegistro.value));
     console.log(this.resultadoRegistro);
 
-    /*switch(this.resultadoRegistro.resultado){
+    switch(this.resultadoRegistro.resultado){
       case("exito"):
           alert("Usuario Registrado con éxito");
           break;
@@ -49,8 +59,7 @@ export class LoginRegistroComponent implements OnInit {
       case("num_argumentos"):
           alert("El número de argumentos pasados es menor de lo posible");
           break;
-      
-      }*/
+      }
     
   }
 
